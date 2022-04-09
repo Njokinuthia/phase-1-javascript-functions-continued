f2da319d22e1f4415dd74bb18d420b0dcd70d013
Njoki <kinuthiash@gmail.com>
2022-04-09 19:39:20 +0300

    add monday work function

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ function saturdayFun(activity='roller-skate'){
   return `This Saturday, I want to ${activity}!`
 }
 console.log(saturdayFun())
+
+// monday work functions
+function mondayWork(activity='go to the office'){
+  return `This Monday, I will ${activity}.`
+}
+console.log(mondayWork(''))
+
