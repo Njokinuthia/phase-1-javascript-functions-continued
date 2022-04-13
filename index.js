54b16cc6fe62c6d10edbdb627442ba0b86d89765
Njoki <kinuthiash@gmail.com>
2022-04-13 09:37:50 +0300

    commit tree dadaf37b497d72d29b876c38d202896ecc5dc7af

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-// saturday fun function
+// saturday fun - function declaration
 function saturdayFun(activity='roller-skate'){
   return `This Saturday, I want to ${activity}!`
 }
 console.log(saturdayFun())
 
-// monday work functions
-function mondayWork(activity='go to the office'){
+// monday work - function expression
+const mondayWork = function(activity='go to the office'){
   return `This Monday, I will ${activity}.`
 }
-console.log(mondayWork(''))
+console.log(mondayWork())
+
+
+// wrap adjective
+
+let wrapAdjective = function(style="*") {
+  return function(adjective="special") {
+    return `You are ${style}${adjective}${style}!`
+  }
+}
+
 
